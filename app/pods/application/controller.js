42193292d84bbf1f25ae8c92c95be267abb670b3
Matthew Coker <matt.james.coker@gmail.com>
2017-10-12 21:22:25 -0500

    Convert image URLs to use local asset paths

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   images: [
-    'https://cdn-images-1.medium.com/max/1600/1*g6s1lvpfArJGorALkKNhvw.png',
-    'https://upload.wikimedia.org/wikipedia/en/6/69/Ember.js_Logo_and_Mascot.png',
-    'https://angular.io/assets/images/logos/angular/angular.png',
-    'https://vuejs.org/images/logo.png',
-    'https://www.polymer-project.org/images/logos/p-author-logo.png',
-    'https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/102012/backbone.png?itok=R-cQvi5L'
+    '/assets/images/react.png',
+    '/assets/images/ember.png',
+    '/assets/images/angular.png',
+    '/assets/images/vue.png',
+    '/assets/images/polymer.png',
+    '/assets/images/backbone.png'
   ],
 
   randomizedCards: Ember.computed('images.length', function() {
