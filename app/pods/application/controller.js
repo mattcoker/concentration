60bf4f0f3ab854b3173f31e88ff49c77a94262d7
Matthew Coker <matt.james.coker@gmail.com>
2017-10-12 22:36:03 -0500

    Add application controller tests for various computed properties and actions, update application controller with necessary computed properties and data structures

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -12,10 +12,49 @@ export default Ember.Controller.extend({
   score: 0,
   attempts: 0,
 
-  randomizedCards: Ember.computed('images.length', function() {
+  randomizedCards: Ember.computed(function() {
     let images = this.get('images'),
         duplicatedImages = images.concat(images);
 
     return duplicatedImages.sort(() => { return 0.5 - Math.random() });
-  })
+  }),
+
+  cardsWithMetadata: Ember.computed('randomizedCards.[]', function() {
+    return this.get('randomizedCards').map((image, index) => {
+      return Ember.Object.create({
+        index: index,
+        image: image,
+        reveal: false,
+        match: false
+      });
+    })
+  }),
+
+  numCards: Ember.computed.reads('cardsWithMetadata.length'),
+
+  gameOver: Ember.computed('cardsWithMetadata.@each.match', function() {
+    let numCards        = this.get('numCards'),
+        numMatchedCards = this.get('cardsWithMetadata').filterBy('match', true).get('length');
+
+    return numMatchedCards === numCards;
+  }),
+
+  statusMessage: Ember.computed('gameOver', 'score', 'attempts', function() {
+    let { gameOver, score, attempts, numCards} = this.getProperties('gameOver', 'score', 'attempts', 'numCards'),
+        maxScore = numCards / 2;
+    if (gameOver) return `Game Over! You finished with a score of ${score} out of a possible ${maxScore}`;
+    return `Score: ${score}, Attempts: ${attempts}`;
+  }),
+
+  actions: {
+    updateScore(delta) {
+      let currScore = this.get('score');
+      this.set('score', currScore + delta);
+      this.incrementProperty('attempts');
+    },
+
+    reset() {
+      window.location.reload(true);
+    }
+  }
 });
--- tests/unit/pods/application/controller-test.js
+++ tests/unit/pods/application/controller-test.js
@@ -11,7 +11,7 @@ test('it exists', function(assert) {
   assert.ok(controller);
 });
 
-test('controller initializes with expected properties ', function(assert) {
+test('controller initializes with expected properties', function(assert) {
   let controller = this.subject(),
       images = controller.get('images'),
       initialScore = 0,
@@ -21,4 +21,87 @@ test('controller initializes with expected properties ', function(assert) {
   assert.equal(images.length, numImages, `Controller initializes with ${numImages} images`);
   assert.equal(controller.get('score'), initialScore, `Controller initializes with "score" at ${initialScore}`);
   assert.equal(controller.get('score'), initialAttempts, `Controller initializes with "attempts" at ${initialAttempts}`);
+});
+
+test('randomizedCards is not just a duplicated images array', function(assert) {
+  let controller = this.subject(),
+    { images, randomizedCards} = controller.getProperties('images', 'randomizedCards');
+
+  // Duplicate images on itself to "verify" that randomizedCards is random
+  images = images.concat(images);
+
+  assert.notDeepEqual(images, randomizedCards, `randomizedCards computed property is actually random`);
+});
+
+test('cardsWithMetadata contains the necessary data struture', function(assert) {
+  let controller = this.subject(),
+      cardsWithMetadata = controller.get('cardsWithMetadata'),
+      haveIndexes = true,
+      haveImages = true,
+      revealIsFalse = true,
+      matchIsFalse = true;
+
+  cardsWithMetadata.forEach((card) => {
+    let props = Object.keys(card);
+    if (!props.includes('index')) haveIndexes = false;
+    if (!props.includes('image')) haveImages = false;
+    if (card.get('reveal')) revealIsFalse = false;
+    if (card.get('match')) matchIsFalse = false;
+  })
+
+  assert.equal(haveIndexes, true, `All cards have an "index" property`);
+  assert.equal(haveImages, true, `All cards have an "images" property`);
+  assert.equal(revealIsFalse, true, `All cards have a "reveal" property set to false`);
+  assert.equal(matchIsFalse, true, `All cards have a "match" property set to false`);
+});
+
+test('numCards matches the length of cardsWithMetadata', function(assert) {
+  let controller = this.subject();
+
+  assert.equal(controller.get('cardsWithMetadata.length'), controller.get('numCards'), 'numCards property matches total length of cardsWithMetadata');
+});
+
+test('gameOver computed property is triggered when all cards have been matched', function(assert) {
+  let controller = this.subject(),
+      cardsWithMetadata = controller.get('cardsWithMetadata');
+
+  assert.equal(controller.get('gameOver'), false, 'gameOver intitializes as false');
+
+  cardsWithMetadata.set('firstObject.match', true);
+  cardsWithMetadata.set('lastObject.match', true);
+  assert.equal(controller.get('gameOver'), false, 'gameOver remains false when some (but not all) cards are matched');
+
+  cardsWithMetadata = cardsWithMetadata.map((card) => {
+    card.set('match', true);
+    return card;
+  });
+
+  assert.equal(controller.get('gameOver'), true, 'gameOver is true when all cards are matched');
+});
+
+test('statusMessage reflects the current state of the game', function(assert) {
+  let controller = this.subject(),
+      cardsWithMetadata = controller.get('cardsWithMetadata');
+
+  assert.equal(controller.get('statusMessage'), 'Score: 0, Attempts: 0', 'statusMessage initializes by displaying score and number of attempts');
+
+  cardsWithMetadata = cardsWithMetadata.map((card) => {
+    card.set('match', true);
+    return card;
+  });
+
+  assert.equal(controller.get('statusMessage'), 'Game Over! You finished with a score of 0 out of a possible 6', 'statusMessage displays final score when all cards are matched');
+});
+
+test('updateScore modifies game state', function(assert) {
+  let controller = this.subject();
+
+  assert.equal(controller.get('score'), 0, '"score" property inintializes at 0');
+  assert.equal(controller.get('attempts'), 0, '"attempts" property inintializes at 0');
+
+  let expectedScore = 5;
+  controller.send('updateScore', expectedScore);
+
+  assert.equal(controller.get('score'), expectedScore, `"score" property is increased to ${expectedScore}`);
+  assert.equal(controller.get('attempts'), 1, '"attempts" property is increased to 1');
 });
