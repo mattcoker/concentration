c846345c3a4405a0ae965b76cc49e8516116e99a
Matthew Coker <matt.james.coker@gmail.com>
2017-10-12 21:11:47 -0500

    Remove disabled flag and selectedCards filterBy computed property, add logic and small helper functions

--- app/pods/components/flip-grid/component.js
+++ app/pods/components/flip-grid/component.js
@@ -2,7 +2,56 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['ui','four','stackable','cards'],
-  disabled: false,
 
-  selectedCards: Ember.computed.filterBy('cards', 'reveal', true),
+  selectedCards: Ember.computed(function() {
+    // Create new array for each instance of the grid component
+    return [];
+  }),
+
+  checkMatch(firstCard, secondCard) {
+    return firstCard.get('image') === secondCard.get('image');
+  },
+
+  setHidden(firstCard, secondCard) {
+    firstCard.set('reveal', false);
+    secondCard.set('reveal', false);
+  },
+
+  setMatched(firstCard, secondCard) {
+    firstCard.set('match', true);
+    secondCard.set('match', true);
+  },
+
+  actions: {
+    selectCard: function(selectedCard) {
+      const selectedCards = this.get('selectedCards'),
+            firstSelected = selectedCards.get('firstObject');
+
+      if (selectedCards.length > 2) {
+        return;
+      }
+
+      selectedCard.set('reveal', true);
+      selectedCards.pushObject(selectedCard);
+
+      if( selectedCards.length < 2 ) {
+        // No need to check for matches if we don't have 2 items selected
+        return;
+      }
+
+      Ember.run.later(() => {
+        let scoreDelta = -.25;
+        // Pair has been selected, set reveal to false for all selected cards
+        this.setHidden(firstSelected, selectedCard);
+
+        if (this.checkMatch(firstSelected, selectedCard)) {
+          this.setMatched(firstSelected, selectedCard);
+          scoreDelta = 1;
+        }
+
+        this.trigger('updateScore', scoreDelta);
+        selectedCards.clear();
+      }, 500);
+    }
+  }
 });
